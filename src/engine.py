6941fd6b3fa0952a760ae519411889aaec4ba7a9
liupold <rohnch@protonmail.ch>
2020-06-25 12:59:43 +0530

    resume is now functional.

--- src/engine.py
+++ src/engine.py
@@ -10,11 +10,11 @@ class Engine():
     """
     Para Downloader Engine.
     """
-    def __init__(self, URL:str, PartFileLocation:str, UserAgent: str, \
+    def __init__(self, URL:str, PartFilePath:str, UserAgent: str, \
             MaxConnection=8, MaxTries=10):
         self.url = URL
         self.max_conn = MaxConnection
-        self.filepath = FilePath # for part files
+        self.partpath = PartFilePath # for part files
         self.agent = UserAgent
         self.session = reqst.Session()
         self.session.headers = {'User-Agent': UserAgent}
@@ -24,9 +24,10 @@ class Engine():
         self.lock = Lock()
         self.__prepared = 0 # Make sure it's prepared.
         self.__killed = False # threads will check this
-        self.__part_files = [] #name of all the part files
+        self.__partpaths = [] #name of all the part files
         self.size = None
         self.chunk_size = None
+
 
     def prepare(self):
         self.part_prefix = \
@@ -40,7 +41,7 @@ class Engine():
             if self.chunkable:
                 self.chunk_size = self.length // self.max_conn
         else:
-            self.length = False
+            self.length = None
             self.chunkable = False
         req.close()
 
@@ -49,24 +50,38 @@ class Engine():
             for conn_num in range(self.max_conn):
                 upper = self.length - (conn_num * self.chunk_size)
                 lower = upper - self.chunk_size + 1
-                part_file = os.path.join(self.filepath, \
+                partpath = os.path.join(self.partpath, \
                         f'{self.part_prefix}.{conn_num}.part')
-                self.__part_files.append(part_file)
+                self.__partpaths.append(partpath)
                 if conn_num == (self.max_conn -  1):
                     lower = 0
+                # resume
+                if os.path.isfile(partpath):
+                    file_length = os.path.getsize(partpath)
+                    if file_length < (upper - lower):
+                        lower += file_length
+                        self.done += file_length
+                    elif (file_length == upper - lower + 1) or \
+                            (file_length == upper - lower):
+                        # this means the part is completed
+                        self.done += file_length
+                        continue # do not append to thread
+                    else:
+                        raise RuntimeError("File Corrupted!")
                 self.threads.append(Thread(target=self.__download_chunk, \
-                        args=(part_file, (lower, upper)), daemon=True))
+                        args=(partpath, (lower, upper)), daemon=True))
         else:
             # download with only one thread!
-            part_file = os.path.join(self.filepath, \
+            partpath = os.path.join(self.partpath, \
                     f'{self.part_prefix}.part')
-            self.__part_files.append(part_file)
+            self.__partpaths.append(partpath)
             self.threads.append(Thread(target=self.__download_chunk, \
-                    args=(part_file,), daemon=True))
+                    args=(partpath,), daemon=True))
 
-        self.__part_files.reverse()
+        self.__partpaths.reverse()
         self.__prepared = 1
         return True
+
 
     def is_active(self):
         for th in self.threads:
@@ -74,44 +89,48 @@ class Engine():
                 return True
         return False
 
-    # this functiom save given chunk into the part file
+    # this function save given chunk into the part file
     # if range is not given download the whole range
-    def __download_chunk(self, filepath, int_range_=(None, None)):
+    def __download_chunk(self, partpath, int_range_=(None, None)):
         if not None in int_range_:
             lower, upper = int_range_
             range_ = {'Range':f'bytes={lower}-{upper}'}
+            print(range_)
         else:
             range_ = {}
-        req = self.session.get(self.url, headers=range_,stream=True)
-        with open(filepath, 'ab+') as f:
+        req = self.session.get(self.url, headers=range_, stream=True)
+        with open(partpath, 'ab+') as f:
             for data in req.iter_content(chunk_size=4096):
                 len_ = f.write(data)
                 with self.lock:
                     self.done += len_
-                    if self.__killed:
-                        return 1
+                    if self.__killed: return 1
+
 
     def download(self, block=True):
         if not self.__prepared:
-            raise ValueError("Download was never prepared!..")
+            self.prepare()
 
         self.__killed =  False # Alive again
         for th in self.threads:
             th.start()
 
         if block:
-            for th in self.threads:
-                th.join()
+            self.join()
             return self.threads
         else:
             return self.threads
 
+    def join(self):
+        for th in self.threads:
+            th.join()
+
     def stop(self):
-        if self.is_active:
+        if self.is_active():
             self.__killed = True
-            for th in self.threads:
-                th.join()
+            self.join()
             self.__prepared = 0
+
 
     def save(self, FilePath):
         if self.is_active():
@@ -121,38 +140,38 @@ class Engine():
             raise RuntimeError('Download was killed! \
                     complete the download before calling self.save')
         elif (self.length is not None) and (self.done != self.length):
+            print(self.done, self.length)
             raise ValueError('Incomplete Download! \
                     complete the download before calling self.save')
         else:
             w_len = 0
             with open(FilePath, 'wb') as F:
-                for part_file in self.__part_files:
-                    with open(part_file, 'rb') as f:
-                        for data in f:
-                            w_len += F.write(data)
+                for partpath in self.__partpaths:
+                    with open(partpath, 'rb') as f:
+                        for data in f: w_len += F.write(data)
 
     def clean(self):
         if self.is_active():
             self.stop()
 
         if self.chunk_size is not None:
-            for part_file in self.__part_files:
-                if os.path.isfile(part_file):
-                    os.remove(part_file)
+            for partpath in self.__partpaths:
+                if os.path.isfile(partpath):
+                    os.remove(partpath)
 
     def __repr__(self):
         return 'awget Engine: ' + str(self.__dict__)
 
 
 URL = 'https://google.com'
-URL = 'http://www.ovh.net/files/1Gb.dat'
-FilePath = './Test/'
+URL = 'https://speed.hetzner.de/100MB.bin'
+tmpPath = './Test/'
 UserAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0'
 
-dl = Engine(URL, FilePath, UserAgent)
+dl = Engine(URL, tmpPath, UserAgent)
 print(dl.is_active())
-dl.prepare()
+# dl.prepare()
 dl.clean()
 dl.download(True)
-dl.save('1Gb.dat')
-dl.clean()
+dl.save('100MB.bin')
+# dl.clean()
