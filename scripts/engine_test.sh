ce771e0ef86612d988dcaef7be63d3da831d4975
liupold <rohnch@protonmail.ch>
2020-06-27 13:12:20 +0530

    only 3.6+ support, fixed requirements.

--- scripts/engine_test.sh
+++ scripts/engine_test.sh
@@ -5,7 +5,7 @@ curl 'http://www.ovh.net/files/1Gb.dat' > 'tmp-for-awget/1Gb.dat.curl'
 curl 'http://speedtest.tele2.net/100MB.zip' > 'tmp-for-awget/100MB.zip.curl'
 curl 'https://speed.hetzner.de/100MB.bin' > 'tmp-for-awget/100MB.bin.curl'
 
-python -m pip install requests
+python -m pip install requests parameterized
 python 'tests/test_engine.py'; TEST_EXIT="$?"
 rm -rf 'tmp-for-awget'
 exit "$TEST_EXIT"
