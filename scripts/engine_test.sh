2beba3cb12cf53cb0317c306372dddc84af1f65d
liupold <rohnch@protonmail.ch>
2020-06-27 14:34:30 +0530

    fixed typo in scripts.

--- scripts/engine_test.sh
+++ scripts/engine_test.sh
@@ -3,7 +3,7 @@ mkdir 'tmp-for-awget'
 # reference files
 curl 'http://www.ovh.net/files/1Gb.dat' > 'tmp-for-awget/1Gb.dat.curl'
 curl 'http://speedtest.tele2.net/100MB.zip' > 'tmp-for-awget/100MB.zip.curl'
-mcurl 'https://speed.hetzner.de/100MB.bin' > 'tmp-for-awget/100MB.bin.curl'
+curl 'https://speed.hetzner.de/100MB.bin' > 'tmp-for-awget/100MB.bin.curl'
 
 python -m pip install requests parameterized
 python 'tests/test_engine.py'; TEST_EXIT="$?"
